91c7834fe97cfa40798a196c75fef9fcf9d0ba15
kami <815918091@qq.com>
2023-02-05 16:34:21 -0800

    version 1.1

--- Scripts/SceneTransition.cs
+++ Scripts/SceneTransition.cs
@@ -10,13 +10,36 @@ using Yarn.Unity;
 public class SceneTransition : MonoBehaviour
 {
     public CanvasGroup canvasGroup;
+    public Animator animator;
+    private bool animFinished;
+    private bool aniSwitch = true;
     void Awake()
     {
         canvasGroup = GetComponent<CanvasGroup>();
-        canvasGroup.DOFade(0, 5f).From(1).onStepComplete = () =>
-        {
-            canvasGroup.gameObject.SetActive(false);
-            DialogueController.Instance.StartChat("Hello");
-        };
+    }
+    void Update()
+    {
+        var animStateInfo = animator.GetCurrentAnimatorStateInfo (0);
+        var NTime = animStateInfo.normalizedTime;
+ 
+        if(NTime > 1.0f){
+           animFinished = true;
+           OpenConsole();
+        }
+        
+    }
+    void OpenConsole()
+    {
+            if(animFinished && aniSwitch){
+
+                Debug.Log("Animation finished");
+                canvasGroup.DOFade(0, 5f).From(1).onStepComplete = () =>
+                {
+                    canvasGroup.gameObject.SetActive(false);
+                    DialogueController.Instance.StartChat("Hello");
+                };
+                animFinished = false;
+                aniSwitch = false;
+            }
     }
 }
